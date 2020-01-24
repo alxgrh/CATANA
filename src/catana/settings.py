d1fc10e2f569efbcde6e44ac93a1f41828c1daa5
Alexey Grachev <alxgrh@yandex.ru>
2020-01-24 18:56:18 +0300

    docker and settings module change

--- src/catana/settings.py
+++ src/catana/settings.py
@@ -1,10 +1,12 @@
-DB_NAME = 'catana'
-DB_USER = 'catana'
-DB_PASSWORD = 'somesecret'
-DB_HOST = '127.0.0.1'
-DB_PORT = '33306'
+import os
 
-DB_ENGINE = 'mysql'
+DB_NAME = os.environ.get( 'CATANA_DB_NAME' , 'catana')
+DB_USER = os.environ.get( 'CATANA_DB_USER' , 'catana')
+DB_PASSWORD = os.environ.get( 'CATANA_DB_PASSWORD' , 'somesecret')
+DB_HOST = os.environ.get( 'CATANA_DB_HOST' , '127.0.0.1')
+DB_PORT = os.environ.get( 'CATANA_DB_PORT' , '33306')
 
-YOUTUBE_API_KEY = 'AIzaSyDda_-kZe4i0U_gUwoFkHmlG8UAIUv2c1o'
-YOUTUBE_CHANNEL_LIST = 'test_channels.json'
+DB_ENGINE = os.environ.get( 'CATANA_DB_ENGINE' , 'mysql')
+
+YOUTUBE_API_KEY =  os.environ.get('YOUTUBE_API_KEY', 'AIzaSyDda_-kZe4i0U_gUwoFkHmlG8UAIUv2c1o')
+YOUTUBE_CHANNEL_LIST =  os.environ.get('YOUTUBE_CHANNEL_LIST', 'test_channels.json')
